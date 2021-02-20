c0db1344071544b059fdcf931adbd17ba9455fed
LucienLuc <lucienluc@umail.ucsb.edu>
2021-02-20 14:04:37 -0800

    Remakign API key constants file

--- bus/bus/busApp.swift
+++ bus/bus/busApp.swift
@@ -9,26 +9,15 @@ import SwiftUI
 import GoogleMaps
 import GooglePlaces
 
-
 class AppDelegate: NSObject, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        var keys: NSDictionary?
-
-        if let path = Bundle.main.path(forResource: "Keys", ofType: "plist") {
-                keys = NSDictionary(contentsOfFile: path)
-            }
-        if let dict = keys {
-            let googlecloudkey = dict["googlecloudapi"] as? String
-            GMSServices.provideAPIKey("\(String(describing: googlecloudkey))")
-            GMSPlacesClient.provideAPIKey("\(String(describing: googlecloudkey))")
+        GMSServices.provideAPIKey(Keys.googleapikey)
+        GMSPlacesClient.provideAPIKey(Keys.googleapikey)
             return true
-               // Initialize Parse.
-               // Parse.setApplicationId(applicationId!, clientKey: clientKey!)
-           }
-        return false
+           
     }
 }
 
